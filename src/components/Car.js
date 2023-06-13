8eb0293009cf4b7b8f3d73bb61370ff7b0f73f87
ineschaouachi <ines.chaouachi@esprit.tn>
2023-06-13 15:49:03 +0100

    First commit

---
+++ src/components/Car.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import car from '../../images/car.png'
+
+const Car = () => {
+  return (
+
+        <img src={car} alt="Car PNG"></img>
+   
+  )
+}
+
+export default Car
---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import Car from "./Car";
+
+
+class Form extends Component{
+    render(){
+        return (
+           
+            <div>
+                <Car />
+                
+                 <h1>Commentaire</h1>
+                 Formulaire
+            </div>
+        )
+    }
+}
+export default Form;
